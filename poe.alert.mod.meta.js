2a9fff30a2a22df910b754adaa83d80b6df1b680
Mark Harvey <mharvey@compassion.com.au>
2016-05-14 01:23:41 +1000

    Update poe.alert.mod.meta.js

--- poe.alert.mod.meta.js
+++ poe.alert.mod.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            PoE.Trade Alert Mod
 // @namespace       https://github.com/crimsonfalconer/poe.alert.mod
-// @version         3
+// @version         4
 // @author          Crimsonfalconer
 // @description     Modify the poe.trade live alert sound
 // @include         http://poe.trade/*/live
@@ -10,7 +10,7 @@
 // @grant           GM_log
 // @updateURL       https://github.com/crimsonfalconer/poe.alert.mod/raw/master/poe.alert.mod.meta.js
 // @downloadURL     https://github.com/crimsonfalconer/poe.alert.mod/raw/master/poe.alert.mod.user.js
-// @updateVersion   3
+// @updateVersion   4
 // @homepageURL     https://github.com/crimsonfalconer/poe.alert.mod
 // @supportURL      https://github.com/crimsonfalconer/poe.alert.mod/issues
 // ==/UserScript==
