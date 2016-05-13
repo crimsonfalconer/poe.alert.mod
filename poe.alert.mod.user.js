0decaa87d7fcee7ef31a48d821c8084152df18cf
Mark Harvey <mharvey@compassion.com.au>
2016-05-14 00:53:41 +1000

    Update with settings, and form to change url

--- poe.alert.mod.user.js
+++ poe.alert.mod.user.js
@@ -3,16 +3,56 @@
 // @namespace   poe.trade
 // @include     http://poe.trade/*/live
 // @version     1
-// @grant       none
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_log
 // ==/UserScript==
- 
-var audioLink = 'https://raw.githubusercontent.com/crimsonfalconer/poe.alert.mod/master/alert.mp3';
- 
-var audioObj = document.getElementById('live-audio');
-var sourceObj = audioObj.getElementsByTagName('source');
- 
-if (sourceObj.length > 0)
+
+GM_log('hi');
+
+function UpdateLiveAlertSound()
 {
-  sourceObj[0].setAttribute('src', audioLink);
-  audioObj.load();
+  var alertURL  = GM_getValue("poeTradeAlertSound", 'https://raw.githubusercontent.com/crimsonfalconer/poe.alert.mod/master/alert.mp3');
+  
+  var inputURL = document.getElementById('alert-sound-url');
+  if (typeof inputURL != 'undefined')
+  {
+    if(inputURL.value != alertURL)
+    {
+      GM_log("Updating URL to: " + inputURL.value);   
+      GM_setValue('poeTradeAlertSound', inputURL.value);
+      alertURL = inputURL.value
+    }
+  }
+ 
+  var audioLink = '';
+  var audioObj = document.getElementById('live-audio');
+  var sourceObj = audioObj.getElementsByTagName('source');
+  if (sourceObj.length > 0)
+  {
+    sourceObj[0].setAttribute('src', alertURL);
+    audioObj.load();
+  }
 }
+
+var alertURL = GM_getValue("poeTradeAlertSound", 'https://raw.githubusercontent.com/crimsonfalconer/poe.alert.mod/master/alert.mp3');
+var panel = document.getElementById('live-notification-settings');
+
+panel.appendChild(document.createElement('br'));
+panel.appendChild(document.createElement('br'));
+
+var link = document.createElement('input');
+link.style = "width:500px";
+link.id = 'alert-sound-url';
+link.type = 'textbox';
+link.value = alertURL;
+panel.appendChild(link);
+
+var save = document.createElement('input');
+save.type = 'button';
+save.value = 'Update Sound';
+save.onclick = UpdateLiveAlertSound;
+panel.appendChild(save);
+
+  
+UpdateLiveAlertSound();
