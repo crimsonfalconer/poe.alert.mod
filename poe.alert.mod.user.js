c8a8c957aec8de17cb1dcd9d90c30a687ee8e7f4
Mark Harvey <mharvey@compassion.com.au>
2016-05-14 01:19:04 +1000

    Update poe.alert.mod.user.js

--- poe.alert.mod.user.js
+++ poe.alert.mod.user.js
@@ -6,6 +6,11 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_log
+// @updateURL       https://github.com/crimsonfalconer/poe.alert.mod/raw/master/poe.alert.mod.meta.js
+// @downloadURL     https://github.com/crimsonfalconer/poe.alert.mod/raw/master/poe.alert.mod.user.js
+// @updateVersion   2
+// @homepageURL     https://github.com/crimsonfalconer/poe.alert.mod
+// @supportURL      https://github.com/crimsonfalconer/poe.alert.mod/issues
 // ==/UserScript==
 
 function UpdateLiveAlertSound()
